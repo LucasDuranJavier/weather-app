8f9bb1e970d4e39d5a7103d87fe4da6bd1772c77
LucasDuranJavier <lucasduranj93@gmail.com>
2022-09-16 15:42:31 -0300

    add images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,13 @@ import CardTemp from "./CardTemp";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import icon_09d from "../assets/09d.png";
 import icon_03n from "../assets/03n.png";
+import icon_04d from "../assets/04d.png";
+import icon_09n from "../assets/09n.png";
+import icon_10d from "../assets/10d.png";
+import icon_11d from "../assets/11d.png";
+import icon_13d from "../assets/13d.png";
+import icon_50d from "../assets/50d.png";
+import icon_02d from "../assets/02d.png";
 import styles from "./Card.module.css";
 
 export default function Card({ max, min, name, img, onClose }) {
@@ -26,9 +33,32 @@ export default function Card({ max, min, name, img, onClose }) {
 
   function WeatherIcon({ icon }) {
     switch (icon) {
+      case "02d":
+        return <img src={icon_02d} alt="few clouds" />;
+
+
+      case "04d":
+        return <img src={icon_04d} alt="broken clouds" />;
+
+        case "09n":
+          return <img src={icon_09n} alt="shower rain" />;
+
+          case "10d":
+            return <img src={icon_10d} alt="rain" />;
+
+            case "11d":
+              return <img src={icon_11d} alt="thunderstorm" />;
+
+              
+              case "13d":
+                return <img src={icon_13d} alt="snow" />;       
+
+                case "50d":
+                  return <img src={icon_50d} alt="mist" />;
+
       case "03n":
-      return <img src={icon_03n} alt="shower rain" />;
+      return <img src={icon_03n} alt="scattered clouds" />;
       default:
-        return <img src={icon_09d} alt="shower rain" />;
+        return <img src={icon_03n} alt="scattered clouds" />;
     }
   }
