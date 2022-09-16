9f7a2b86e449280b45c14b120b2937e5c3ccd832
LucasDuranJavier <lucasduranj93@gmail.com>
2022-09-16 15:43:22 -0300

    commit form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 import { IoSearchOutline } from "react-icons/io5";
+
 import styles from "./SearchBar.module.css";
-export default function SearchBar({onSearch}) {
+
+export default function SearchBar({ onSearch }) {
   // acá va tu código
-  function handleOnSearch() {
+  function handleOnSearch(event) {
+    event.preventDefault();
     if (typeof onSearch === "function") {
-     const input = document.getElementById("search-bar-input");
-     onSearch(input.value)
+      const input = document.getElementById("search-bar-input");
+      onSearch(input.value);
+      input.value = "";
     }
   }
+
   return (
     <form className={styles.searchBar} onSubmit={handleOnSearch}>
       <input placeholder="Agrega una nueva ciudad..." id="search-bar-input" />
